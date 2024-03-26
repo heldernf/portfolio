a9085d9889374ed9e5816269264d7bd857c69e63
Helder Neto <helderneto2005@gmail.com>
2024-03-25 23:35:32 -0300

    Reformulação da lógica para o slide
    Agora a lógica do slide é feita com base em objetos e está 100% funcional, faltando apenas a adicão do touch para mobile.

--- script.js
+++ script.js
@@ -57,14 +57,12 @@ main.addEventListener('click', function () {
 });
 
 // ATUALIZA O LINK DO MENU QUE ESTÁ ATIVO
-let timeout;
 function attOption() {
-    clearTimeout(timeout);
-    timeout = setTimeout(function () {
+    setTimeout(function () {
         sections.forEach((section, index) => {
             let topo = section.getBoundingClientRect().top;
             let janela = window.innerHeight;
-
+            
             if (topo < janela - 450) {
                 activeOption(index);
             }
@@ -75,69 +73,66 @@ window.addEventListener('scroll', attOption);
 
 // SLIDE FUNCTIONS ---- SLIDE FUNCTIONS ---- SLIDE FUNCTIONS
 const firstImg = document.querySelectorAll(".first-img-slide");
-const btnSlide = document.querySelectorAll(".slide-button");
-const slideBox = document.getElementsByClassName("slide-box");
-const totalImg = slideBox.length * slideBox[0].getElementsByTagName("img").length;
-let positionSlide = 1;
+const positionSlide = {};
 
-function activeBackground() {
-    btnSlide.forEach(btn => {
-        btn.style.backgroundColor = "transparent";
+//MARCA OS BACKGROUND DOS BOTÕES DO SLIDE
+function activeBackground(i) {
+    const allBtnSlideCurrent = firstImg[i].parentNode.querySelector('.slide-control').querySelectorAll('.slide-button');
+    
+    allBtnSlideCurrent.forEach(btn => {
+        btn.style.backgroundColor = 'transparent';
     });
-    for (let i = positionSlide; i < totalImg; i += 3) {
-        btnSlide[i].style.backgroundColor = "white";
+    
+    let nome = 'slide' + i;
+    if (positionSlide[nome] == -1) {
+        allBtnSlideCurrent[2].style.backgroundColor = 'white'
+    } else {
+        allBtnSlideCurrent[positionSlide[nome]].style.backgroundColor = 'white'
     }
 }
 
 // PASSA IMAGENS AUTOMATICAMENTE
-function moveSlide() {
-    switch (positionSlide) {
-        case 0:
-            firstImg.forEach(img => {
-                img.style.marginLeft = "0%";
-            })
-            activeBackground();
-            positionSlide++;
-            break;
-        case 1:
-            firstImg.forEach(img => {
-                img.style.marginLeft = "-100%";
-            })
-            activeBackground();
-            positionSlide++;
-            break;
-        case 2:
-            firstImg.forEach(img => {
-                img.style.marginLeft = "-200%";
-            })
-            activeBackground();
-            positionSlide = 0;
-            break;
+function moveSlide(nome, i) {
+    if (positionSlide[nome] == 0) {
+        firstImg[i].style.marginLeft = "0%";
+    } else if (positionSlide[nome] == 1) {
+        firstImg[i].style.marginLeft = "-100%";
+    } else {
+        positionSlide[nome] = -1;
+
+        firstImg[i].style.marginLeft = "-200%";
     }
 }
-const intervalo = setInterval(moveSlide, 10000);
+
+function icrementoSlide() {
+    for (let i = 0; i < firstImg.length; i++) {
+        let nome = 'slide' + i;
+
+        if (positionSlide[nome] == undefined) {
+            positionSlide[nome] = 1;
+        } else {
+            positionSlide[nome]++;
+        }
+        
+        moveSlide(nome, i);
+        activeBackground(i);
+    }
+}
+setInterval(icrementoSlide, 8000);
 
 // PASSA IMAGENS DO SLIDE CLICANDO
+const btnSlide = document.querySelectorAll(".slide-button");
 btnSlide.forEach((button, index) => {
     button.addEventListener('click', function () {
-        const img = button.parentNode.parentNode.getElementsByClassName("first-img-slide");
-        const slideControls = button.parentNode.querySelectorAll(".slide-button");
-        const positionSlideDois = index % 3;
+        const allBoxProjeto = document.querySelectorAll('.box-projeto');
+        const currentBoxProjeto = button.closest('.box-projeto');
+        const indexCurrentBoxProjeto = Array.from(allBoxProjeto).indexOf(currentBoxProjeto);
 
-        slideControls.forEach(control => {
-            control.style.backgroundColor = "transparent"
-        });
-
-        if (positionSlideDois == 0) {
-            img[0].style.marginLeft = "0%";
-            btnSlide[index].style.backgroundColor = "white";
-        } else if (positionSlideDois == 1) {
-            img[0].style.marginLeft = "-100%";
-            btnSlide[index].style.backgroundColor = "white";
-        } else {
-            img[0].style.marginLeft = "-200%";
-            btnSlide[index].style.backgroundColor = "white";
-        }
+        const nome = 'slide' + indexCurrentBoxProjeto;
+        const selectPosition = index % 3;
+        positionSlide[nome] = selectPosition;
+        moveSlide(nome, indexCurrentBoxProjeto);
+        activeBackground(indexCurrentBoxProjeto);
     });
 });
 
