3fb2039a84d38b515f8569763531f744e35c289e
Helder Neto <helderneto2005@gmail.com>
2024-03-31 02:15:25 -0300

    reformulação do html/css do formulário

--- php/form.php
+++ php/form.php
@@ -16,26 +16,28 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $mensagem = $_POST['mensagem'];
 
     $corpo_email = "
-        <h1 style=\"color: #6A3ED4; text-align: center; font-weight: 800; margin: 0; padding: 15px; font-size: 30px;\">NOVO EMAIL</h1>
-        <div style=\"width: 90%; border-radius: 4px; overflow: hidden; margin: 0 auto;\">
-            <p style=\"background-color: #6A3ED4; color: #fff; margin: 0; padding: 14px 12px; font-size: 16px; font-weight: 700;\">Nome:</p>
-            <p style=\"background-color: #e2e2e2; color: #6A3ED4; margin: 0; padding: 14px 12px; font-size: 14px; font-weight: 600;\">$nome</p>
-        </div>
-        <div style=\"margin: 15px auto; width: 90%; border-radius: 4px; overflow: hidden;\">
-            <p style=\"background-color: #6A3ED4; color: #fff; margin: 0; padding: 14px 12px; font-size: 16px; font-weight: 700;\">Email:</p>
-            <p style=\"background-color: #e2e2e2; color: #6A3ED4; margin: 0; padding: 14px 12px; font-size: 14px; font-weight: 600;\">$email</p>
-        </div>
-        <div style=\"margin: 15px auto; width: 90%; border-radius: 4px; overflow: hidden;\">
-            <p style=\"background-color: #6A3ED4; color: #fff; margin: 0; padding: 14px 12px; font-size: 16px; font-weight: 700;\">Telefone:</p>
-            <p style=\"background-color: #e2e2e2; color: #6A3ED4; margin: 0; padding: 14px 12px; font-size: 14px; font-weight: 600;\">$telefone</p>
-        </div>
-        <div style=\"margin: 15px auto; width: 90%; border-radius: 4px; overflow: hidden;\">
-            <p style=\"background-color: #6A3ED4; color: #fff; margin: 0; padding: 14px 12px; font-size: 16px; font-weight: 700;\">Assunto:</p>
-            <p style=\"background-color: #e2e2e2; color: #6A3ED4; margin: 0; padding: 14px 12px; font-size: 14px; font-weight: 600;\">$assunto</p>
-        </div>
-        <div style=\"margin: 15px auto; width: 90%; border-radius: 4px; overflow: hidden;\">
-            <p style=\"background-color: #6A3ED4; color: #fff; margin: 0; padding: 14px 12px; font-size: 16px; font-weight: 700;\">Mensagem:</p>
-            <p style=\"background-color: #e2e2e2; color: #6A3ED4; margin: 0; padding: 14px 12px; font-size: 14px; font-weight: 600;\">$mensagem</p>
+        <div style=\"padding-bottom: 15px;\">
+            <h1 style=\"color: #6A3ED4; text-align: center; font-weight: 800; margin: 0; padding: 15px; font-size: 30px;\">NOVO EMAIL</h1>
+            <div style=\"width: 90%; border-radius: 4px; overflow: hidden; margin: 0 auto;\">
+                <p style=\"background-color: #6A3ED4; color: #fff; margin: 0; padding: 14px 12px; font-size: 16px; font-weight: 700;\">Nome:</p>
+                <p style=\"background-color: #e2e2e2; color: #6A3ED4; margin: 0; padding: 14px 12px; font-size: 14px; font-weight: 600;\">$nome</p>
+            </div>
+            <div style=\"margin: 15px auto 0 auto; width: 90%; border-radius: 4px; overflow: hidden;\">
+                <p style=\"background-color: #6A3ED4; color: #fff; margin: 0; padding: 14px 12px; font-size: 16px; font-weight: 700;\">Email:</p>
+                <p style=\"background-color: #e2e2e2; color: #6A3ED4; margin: 0; padding: 14px 12px; font-size: 14px; font-weight: 600;\">$email</p>
+            </div>
+            <div style=\"margin: 15px auto 0 auto; width: 90%; border-radius: 4px; overflow: hidden;\">
+                <p style=\"background-color: #6A3ED4; color: #fff; margin: 0; padding: 14px 12px; font-size: 16px; font-weight: 700;\">Telefone:</p>
+                <p style=\"background-color: #e2e2e2; color: #6A3ED4; margin: 0; padding: 14px 12px; font-size: 14px; font-weight: 600;\">$telefone</p>
+            </div>
+            <div style=\"margin: 15px auto 0 auto; width: 90%; border-radius: 4px; overflow: hidden;\">
+                <p style=\"background-color: #6A3ED4; color: #fff; margin: 0; padding: 14px 12px; font-size: 16px; font-weight: 700;\">Assunto:</p>
+                <p style=\"background-color: #e2e2e2; color: #6A3ED4; margin: 0; padding: 14px 12px; font-size: 14px; font-weight: 600;\">$assunto</p>
+            </div>
+            <div style=\"margin: 15px auto 0 auto; width: 90%; border-radius: 4px; overflow: hidden;\">
+                <p style=\"background-color: #6A3ED4; color: #fff; margin: 0; padding: 14px 12px; font-size: 16px; font-weight: 700;\">Mensagem:</p>
+                <p style=\"background-color: #e2e2e2; color: #6A3ED4; margin: 0; padding: 14px 12px; font-size: 14px; font-weight: 600;\">$mensagem</p>
+            </div>
         </div>
     ";
 
