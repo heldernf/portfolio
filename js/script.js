9b9184e6a123d0c6d4283a554454302225893201
Helder Neto <helderneto2005@gmail.com>
2024-04-08 16:48:31 -0300

    Finalizado e testado, 100% funcionando

--- js/script.js
+++ js/script.js
@@ -193,3 +193,18 @@ form.addEventListener('submit', function () {
     body.style.overflow = 'hidden';
     div.classList.add('load');
 });
+
+// ANIMATION FADE
+const fadeElements = document.querySelectorAll(".fade-up, .fade-right, .fade-left, .fade-boom");
+
+function fade() {
+    fadeElements.forEach(fade => {
+        const fadeType = fade.classList.contains("fade-up") ? "up" : fade.classList.contains("fade-right") ? "right" : fade.classList.contains("fade-left") ? "left" : "boom";
+
+        if (fade.getBoundingClientRect().top - document.querySelector("header").offsetHeight <= 830) {
+            fade.style.animation = `${fadeType} .8s cubic-bezier(0.34, 1.4, 0.64, 1) 1 forwards`;
+        }
+    });
+}
+document.addEventListener("DOMContentLoaded", fade);
+window.addEventListener("scroll", fade);
