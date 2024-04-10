7d8d35f1945614976e7ba74cb085ac99a6a4990c
Helder Neto <helderneto2005@gmail.com>
2024-04-10 04:43:50 -0300

    ddd

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,10 @@ const path = require('path');
 
 module.exports = {
     entry: {
-        main: './assets/js/main.js' // Seu arquivo JavaScript principal
+        main: './assets/js/main.js'
     },
     output: {
-        path: path.resolve(__dirname, 'assets/js/dist'), // Diretório de saída
-        filename: 'bundle.js' // Nome do arquivo de saída
+        path: path.resolve(__dirname, 'assets/js/dist'),
+        filename: 'bundle.js'
     }
 };
