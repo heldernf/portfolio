d2f1803395cbd990ca5d04510906943d79135049
Helder Neto <helderneto2005@gmail.com>
2024-04-12 17:42:01 -0300

    Finalizado
    Falta apenas alguns detalhamentos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
+// JS
 import './fade.js';
 import './load.js';
 import './menu.js';
 import './slide.js';
+// CSS
 import '../css/main.css';
 import '../css/media_queries.css';
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,57 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssWebpackPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const glob = require('glob');
 
 module.exports = {
-    mode: 'production',
-    entry: './assets/js/main.js',
+    mode: 'development',
+    entry: {
+        main: ['./assets/js/main.js', ...getImages()]
+    },
     output: {
-        filename: 'bundle.[contenthash].js',
-        path: path.resolve(__dirname, 'dist')
+        filename: 'assets/js/bundle.[contenthash].js',
+        path: path.resolve(__dirname, 'home')
     },
     module: {
         rules: [
-            { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+            {
+                test: /\.css$/,
+                use: [
+                    { loader: MiniCssWebpackPlugin.loader },
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]',
+                    outputPath: 'assets/img/'
+                }
+            },
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './index.html',
+        }),
+        new MiniCssWebpackPlugin({
+            filename: './assets/css/main.[contenthash].css'
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: './php/', to: 'php' },
+                { from: '.env', to: './'},
+                { from: '.htaccess', to: './'},
+                { from: 'composer.json', to: './'},
+                { from: 'package.json', to: './'},
+            ]
+        })
+    ]
+}
+
+function getImages() {
+    return glob.sync('./assets/img/**/*.+(png|jpg|jpeg|gif|svg)').map(image => path.resolve(__dirname, image));
 }
