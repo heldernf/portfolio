68f9f58604406cbdaffbf47a854bf8d7e1deaf71
Helder Neto <helderneto2005@gmail.com>
2024-04-14 04:17:00 -0300

    Otimização do fade.js
    Eliminado a chamada dele após ele animar todas os elementos que possuem as devidas classes declaradas em const fadeType

--- assets/js/fade.js
+++ assets/js/fade.js
@@ -1,11 +1,20 @@
 const fadeElements = document.querySelectorAll(".fade-up, .fade-right, .fade-left, .fade-boom");
+let counter = 0;
+window.innerWidth < 500 ? lack = 700 : lack = 750;
+
+function stopEventListenerScroll () {
+    window.removeEventListener("scroll", fade);
+}
 
 function fade() {
     fadeElements.forEach(fade => {
         const fadeType = fade.classList.contains("fade-up") ? "up" : fade.classList.contains("fade-right") ? "right" : fade.classList.contains("fade-left") ? "left" : "boom";
 
-        if (fade.getBoundingClientRect().top - document.querySelector("header").offsetHeight <= 830) {
+        if (fade.getBoundingClientRect().top - document.querySelector("header").offsetHeight <= lack && getComputedStyle(fade).getPropertyValue('animation-name') != fadeType) {
             fade.style.animation = `${fadeType} .8s cubic-bezier(0.34, 1.4, 0.64, 1) 1 forwards`;
+            
+            counter++;
+            counter == fadeElements.length && stopEventListenerScroll();
         }
     });
 }
