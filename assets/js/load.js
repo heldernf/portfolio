3d7bccf35f98389a796ce9e96998f811d0aff64e
Helder Neto <helderneto2005@gmail.com>
2026-07-30 19:08:25 -0300

    reset repository

--- assets/js/load.js
+++
@@ -1,9 +0,0 @@
-function load() {
-    const body = document.querySelector("body");
-    const div = document.createElement("div");
-    
-    body.appendChild(div);
-    body.style.overflow = 'hidden';
-    div.classList.add('load');
-}
-document.getElementById("form-contato").addEventListener("submit", load);
--- assets/js/main.js
+++
@@ -1,7 +0,0 @@
-// JS
-import './fade.js';
-import './load.js';
-import './slide.js';
-// CSS
-import '../css/main.css';
-import '../css/media_queries.css';
